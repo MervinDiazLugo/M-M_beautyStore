3db483fc5bb79a92a1df60b175d185c37a1745a6
Mervin Diaz <mervindiazlugo@gmail.com>
2026-01-02 01:27:27 -0300

    sticky adjustment

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
       {/* Contenedor de búsqueda STICKY */}
       <div style={{
         position: 'sticky',
-        top: '80px', // Debajo del header que mide ~80px
+        top: '100px',
         zIndex: 90,
         backgroundColor: '#fff',
         borderBottom: '1px solid #f0f0f0',
